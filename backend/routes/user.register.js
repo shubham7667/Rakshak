b259d74be3ede221f2a0ce5243261fc4c7934631
Shubham Kumar <rishab.ss64@gmail.com>
2025-05-27 13:30:08 +0530

    #

--- backend/routes/user.register.js
+++ backend/routes/user.register.js
@@ -8,12 +8,14 @@ route.post('/register',async(req,res)=>{
     const { username, email, password, phone, state, city, gender } = req.body;
  try{
        if (!username || !email || !password || !phone || !state || !city || !gender) {
+        console.log("Kuch to gadbad hai ")
         return res.status(400).json({ error: 'All fields are required' });
     }
 
    const newuser = await user.create(
     {username, email, password, phone, state, city, gender}
 )
+console.log("Kam to hogya")
 // console.log(newuser)   
     res.status(200).json({message:'registration successfull'})
  
